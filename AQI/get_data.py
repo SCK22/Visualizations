21f40c886d72d6c8911976c5c75374f1097fd3ee
sck22 <chaithanyakumar.ds@gmail.com>
2019-09-27 14:17:37 +0530

    multiple files

--- AQI/get_data.py
+++ AQI/get_data.py
@@ -3,33 +3,32 @@ import pandas as pd
 
 
 offset = 0
-
+with open("keys/data_gov_api_key.txt") as f:
+    api_key = f.read()
+print("api_key",api_key)
 base_url  = "https://api.data.gov.in/resource/3b01bcb8-0b14-4abf-b6f2-c1bfd384ba69"
-api_key = "Your API Key"
+# api_key = "Your API Key"
 data_format = "json"
 offset = 0
 
-url = "{}?api-key={}&format={}&offset={}".format(base_url,api_key,data_format, offset)
-
+# url = "{}?api-key={}&format={}&offset={}".format(base_url,api_key,data_format, offset)
 data = []
-
 retrieved_rows = 1
-while retrieved_rows < 200000:
-# print(url)
-    url = "{}?{}&format={}&offset={}".format(base_url,api_key,data_format, offset)
+while retrieved_rows < 100000:
+    
+    url = "{}?api-key={}&format={}&offset={}".format(base_url,api_key,data_format, offset)
     r = requests.get(url)
-    # print (r)
     response = r.json()
-    # print(data['records'])
     retrieved_rows += len(response['records'])
-    # if retrieved_rows == 0 :
-    #     break
-    # else:
     data.extend(response['records'])
     offset = 100 #len(response['records'])
     print("retrieved_rows", retrieved_rows)
 
 df = pd.DataFrame(data)  
-print(df.head())
-df.to_csv("aqi.csv")
+# print(df.head())
+last_update = df.last_update[0]
+last_update = "-".join(last_update.split(" "))
+last_update = "-".join(last_update.split(":"))
+print(last_update)
+df.to_csv("data/aqi-{}.csv".format(last_update))
 
